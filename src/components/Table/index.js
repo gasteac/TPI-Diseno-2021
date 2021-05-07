eacea1e69b1034ee38b39e35793b692c108469cf
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2021-05-07 19:23:04 -0300

    tabla

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,6 +1,13 @@
 import { useMemo } from "react";
-import "./index.css"
-import { useTable, useSortBy, useGlobalFilter, useFilters, usePagination, useRowSelect } from "react-table";
+import "./index.css";
+import {
+  useTable,
+  useSortBy,
+  useGlobalFilter,
+  useFilters,
+  usePagination,
+  useRowSelect,
+} from "react-table";
 import GlobalFilter from "./components/GlobalFilter";
 import ColumnFilter from "./components/ColumnFilter";
 import { Checkbox } from "./components/Checkbox";
@@ -8,117 +15,145 @@ import Delete from "./components/Delete";
 import ModifyState from "./components/ModifyState";
 
 export default function Table({ columnas, datos }) {
+  const columns = useMemo(() => columnas, []);
+  const data = useMemo(() => datos, []);
 
-    const columns = useMemo(() => columnas, [])
-    const data = useMemo(() => datos, [])
+  const defaultColumn = useMemo(() => {
+    return {
+      Filter: ColumnFilter,
+    };
+  }, []);
 
-    const defaultColumn = useMemo(() => {
-        return {
-            Filter: ColumnFilter,
-        }
-    }, [])
-
-    const tableInstance = useTable({
-        columns,
-        data,
-        defaultColumn,
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
     },
-        useFilters, useGlobalFilter, useSortBy, usePagination, useRowSelect,
-        (hooks) => {
-            hooks.visibleColumns.push((columns) => {
-                return [
-                    {
-                        id: 'selection',
-                        Header: ({ getToggleAllRowsSelectedProps }) => (
-                            <Checkbox {...getToggleAllRowsSelectedProps()} />
-                        ),
-                        Cell: ({ row }) => (
-                            <Checkbox {...row.getToggleRowSelectedProps()} />
-                        )
-                    },
-                    ...columns,
-                ]
-            })
-        }
-    )
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination,
+    useRowSelect,
+    (hooks) => {
+      hooks.visibleColumns.push((columns) => {
+        return [
+          {
+            id: "selection",
+            Header: ({ getToggleAllRowsSelectedProps }) => (
+              <Checkbox {...getToggleAllRowsSelectedProps()} />
+            ),
+            Cell: ({ row }) => (
+              <Checkbox {...row.getToggleRowSelectedProps()} />
+            ),
+          },
+          ...columns,
+        ];
+      });
+    }
+  );
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        page,
-        prepareRow,
-        state,
-        setGlobalFilter,
-        nextPage,
-        previousPage,
-        canNextPage,
-        canPreviousPage,
-        pageOptions,
-        selectedFlatRows
-    } = tableInstance
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    state,
+    setGlobalFilter,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    selectedFlatRows,
+  } = tableInstance;
 
-    const { globalFilter, pageIndex } = state
+  const { globalFilter, pageIndex } = state;
 
-    return (
-        <>
-            <div className='contenedor-propio'>
-                <GlobalFilter  filter={globalFilter} setFilter={setGlobalFilter} />
-                <div >
-                    <table {...getTableProps()} className='table tabla-propia'>
-                        <thead className='tabla-header-propio'>
-                            {
-                                headerGroups.map(headerGroup => (
-                                    <tr {...headerGroup.getHeaderGroupProps()}>
-                                        {headerGroup.headers.map(column => (
-                                            <th scope="col" className='th-propio'{...column.getHeaderProps(column.getSortByToggleProps())}>
-                                                <div> {column.render('Header')}
-                                                <span>
-                                                    {column.isSorted ? (column.isSortedDesc ? ' ↓' : ' ↑') : ' -'}
-                                                </span>
-                                                </div>
-                                                <div>{column.canFilter ? column.render('Filter') : null}</div>
-                                            </th>
-                                        ))}
-                                    </tr>
-                                ))
-                            }
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <div style={{overflowX: "auto"}}>
+        <table {...getTableProps()} className="table tabla-propia">
+          <thead className="tabla-header-propio">
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    scope="col"
+                    className="th-propio"
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                  >
+                    <div>
+                      {" "}
+                      {column.render("Header")}
+                      <span>
+                        {column.isSorted
+                          ? column.isSortedDesc
+                            ? " ↓"
+                            : " ↑"
+                          : " -"}
+                      </span>
+                    </div>
+                    <div>
+                      {column.canFilter ? column.render("Filter") : null}
+                    </div>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr className="table-primary" {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
 
-                        </thead>
-                        <tbody {...getTableBodyProps()}>
-                            {
-                                page.map(row => {
-                                    prepareRow(row)
-                                    return (
-                                        <tr className="table-primary" {...row.getRowProps()}>
-                                            {
-                                                row.cells.map(cell => {
-                                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                                })
-                                            }
-                                        </tr>
-                                    )
-                                })
-                            }
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <button
+              className="page-link"
+              onClick={() => previousPage()}
+              disabled={!canPreviousPage}
+            >
+              Previous
+            </button>
+          </li>
+          <li className="page-item">
+            <span className="page-link">
+              Page{" "}
+              <strong>
+                {pageIndex + 1} of {pageOptions.length}
+              </strong>{" "}
+            </span>
+          </li>
 
-                        </tbody>
-                    </table>
-                </div>
+          <li className="page-item">
+            <button
+              className="page-link"
+              onClick={() => nextPage()}
+              disabled={!canNextPage}
+            >
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
 
-                <nav aria-label="Page navigation example">
-                        <ul className="pagination">
-                            <li className="page-item"><button className="page-link" onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button></li>
-                            <li className="page-item"><span className="page-link">
-                                Page{' '}
-                                <strong>{pageIndex + 1} of {pageOptions.length}</strong>{' '}
-                            </span></li>
-
-                            <li className="page-item"><button className="page-link" onClick={() => nextPage()} disabled={!canNextPage}>Next</button></li>
-                        </ul>
-                    </nav>
-
-            </div>
-            {/* <pre>
+      {/* <pre>
                 <code>
                     {JSON.stringify(
                         {
@@ -129,6 +164,6 @@ export default function Table({ columnas, datos }) {
                     )}
                 </code>
             </pre> */}
-        </>
-    );
+    </>
+  );
 }
--- src/layout/usuariosLayout/AgenteInmobiliario/views/Agenda/index.js
+++ src/layout/usuariosLayout/AgenteInmobiliario/views/Agenda/index.js
@@ -1,14 +1,20 @@
 import Layout from "../../../../Layout";
 import Table from "../../../../../components/Table/";
-import MOCK_DATA from './assets/MOCK_DATA.json';
-import { COLUMNS } from './assets/columns';
+import MOCK_DATA from "./assets/MOCK_DATA.json";
+import { COLUMNS } from "./assets/columns";
+import { Container } from "react-bootstrap";
 
 export default function Agenda() {
-    const usuario = 'agente-inmobiliario';
+  const usuario = "agente-inmobiliario";
 
-    return (
-        <Layout items={["Inicio", "Clientes", "Agenda", "Propiedades"]} usuario={usuario}>
-            <Table columnas={COLUMNS} datos={MOCK_DATA}/>
-        </Layout>
-    );
+  return (
+    <Layout
+      items={["Inicio", "Clientes", "Agenda", "Propiedades"]}
+      usuario={usuario}
+    >
+      <Container fluid>
+        <Table columnas={COLUMNS} datos={MOCK_DATA} />
+      </Container>
+    </Layout>
+  );
 }
