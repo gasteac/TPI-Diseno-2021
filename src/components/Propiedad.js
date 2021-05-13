a35ac2b47a9f0029305568f8bbbddd7c3bcc7ce7
gastonwolf <gastoneacosta@outlook.com>
2021-05-13 13:18:33 -0300

    improves

--- src/components/Propiedad.js
+++ src/components/Propiedad.js
@@ -56,12 +56,12 @@ const Propiedad = ({ history }) => {
       <Container style={{marginTop:'20px', marginBottom:'20px'}}>
         <Row>
           <Col xs={7}>
-            <Row >
-              <Image src={theImages[DatosDelInmueble.nroInmueble]} className='img-thumbnail' style={{marginBottom:'20px', width:'700px'}} />
+            <Row className='d-flex justify-content-center'>
+              <Image src={theImages[DatosDelInmueble.nroInmueble]} className='img-thumbnail' style={{marginBottom:'20px', width:'575px'}} />
             </Row>
             <Row>
               <Card className="m-0 p-0" >
-                <Card.Header as="h2">Datos de Contacto</Card.Header>
+                <Card.Header as="h4">Datos de Contacto</Card.Header>
                 <Card.Body>
                   <DatosDeContactoComponent DatosDeContacto={DatosDeContacto} />
                 </Card.Body>
@@ -70,7 +70,7 @@ const Propiedad = ({ history }) => {
           </Col>
           <Col xs={5}>
             <Card>
-              <Card.Header as="h2">Datos del inmueble</Card.Header>
+              <Card.Header as="h4">Datos del inmueble</Card.Header>
               <Card.Body>
                 <DatosDelInmuebleComponent
                   DatosDelInmueble={DatosDelInmueble}
