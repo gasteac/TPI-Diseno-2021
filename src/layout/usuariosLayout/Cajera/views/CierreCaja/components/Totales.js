d5d4d2908852bd4016602c242ea3fdd4c262ca65
gastonchifflets <gastonchifflets@gmail.com>
2021-05-13 22:44:35 -0300

    agregado componente totales, falta implementar en cierre caja

--- src/layout/usuariosLayout/Cajera/views/CierreCaja/components/Totales.js
+++ src/layout/usuariosLayout/Cajera/views/CierreCaja/components/Totales.js
@@ -1,5 +1,17 @@
 export default function Totales({ datos }) {
+
+    const totalEgreso = datos.amount.reduce((prev, current) => {if (datos.type == 'egreso') prev + current})
+
+    const totalIngreso = datos.amount.reduce((prev, current) => {if (datos.type == 'ingreso') prev + current})
+
+    const total = totalEgreso + totalIngreso;
+
+
     return (
-        
+        <>
+          <p>Total ingresos: ${totalIngreso}</p>  
+          <p>Total egresos: ${totalEgreso}</p>  
+          <p>Total general: ${total}</p>  
+        </>
     );
 }
